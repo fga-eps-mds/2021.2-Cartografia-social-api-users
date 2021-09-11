a967869be2143893c72f18b88d481ebde4d6a6e4
Elias Bernardo <ebmm01@gmail.com>
2021-09-11 18:02:12 -0300

    [#6] feat - add userSchema and mongoose imports for DI
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from './config/configuration';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(new ConfigService().get('mongo').url),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,30 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+export enum UserEnum {
+  RESEARCHER,
+  COMMUNITY_MEMBER,
+  ADMIN,
+}
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  cellPhone: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop()
+  imageUrl?: string;
+
+  @Prop({ enum: UserEnum, required: true })
+  type: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
