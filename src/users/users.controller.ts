156b9a17bd03487cfe31b54064698e7d15ebb5fe
Elias Bernardo <ebmm01@gmail.com>
2021-09-12 09:43:04 -0300

    [#6] feat - add create community member route
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,14 +2,36 @@ import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserEnum } from './entities/user.entity';
 
 @Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @MessagePattern('createResearcher')
-  async create(@Payload() createUserDto: CreateUserDto) {
-    const id = await this.usersService.create(createUserDto);
+  async createResearcher(@Payload() createUserDto: CreateUserDto) {
+    const id = await this.usersService.create(
+      createUserDto,
+      UserEnum.RESEARCHER,
+    );
+
     return { id };
+  }
+
+  @MessagePattern('createCommunityMember')
+  async createCommunityMember(@Payload() createUserDto: CreateUserDto) {
+    const id = await this.usersService.create(
+      createUserDto,
+      UserEnum.COMMUNITY_MEMBER,
+    );
+
+    return { id };
+  }
+
+  @MessagePattern('getUserData')
+  async getUserData(@Payload() email: string) {
+    const response = await this.usersService.getUserByEmail(email);
+
+    return response.toJSON();
   }
 }
