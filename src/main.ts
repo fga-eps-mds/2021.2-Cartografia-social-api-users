8656f7d6e66136973d7047de50920b46f48cb7a7
Marcos N <marcosnery.comp@gmail.com>
2022-04-08 04:26:36 -0300

    Adding support for https endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 import { ConfigService } from './config/configuration';
 
 type MicrosserviceConfig = {
@@ -13,20 +13,20 @@ async function bootstrap() {
     'microsservice',
   );
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: [microsserviceOptions.host],
-        queue: microsserviceOptions.queueName,
-        queueOptions: {
-          durable: true,
-        },
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [microsserviceOptions.host],
+      queue: microsserviceOptions.queueName,
+      queueOptions: {
+        durable: true,
       },
     },
-  );
+  });
 
-  await app.listen();
+  await app.startAllMicroservices();
+
+  await app.listen(9880);
 }
 bootstrap();
